c185bbb8365e0abb1ac7bbfd2cd2ef73aaa6edd8
Nelson LeDuc <wolfon33@gmail.com>
2014-09-16 18:13:43 -0500

    Begin implementing swift, convert the app delegate

---
+++ DriveDetector/DDAppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  DDAppDelegate.swift
+//  DriveDetector
+//
+//  Created by Nelson LeDuc on 9/16/14.
+//  Copyright (c) 2014 Nelson LeDuc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class DDAppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+    var driveController: DDDriveController!
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool
+    {
+        self.driveController = DDDriveController()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        var driveViewController = storyboard.instantiateInitialViewController() as DDViewController
+        driveViewController.setDetector(self.driveController.driveDetector)
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = driveViewController//.setRootViewController(driveViewController)
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
